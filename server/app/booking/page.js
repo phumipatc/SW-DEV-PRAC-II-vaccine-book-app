5d95bc30c29a4557940ecb45c053d62cb5e19512
Phumipat Chaiprasertsud <phumipat.c@outlook.com>
2023-09-09 14:17:28 +0700

    Updates

--- server/app/booking/page.js
+++ server/app/booking/page.js
@@ -342,6 +342,18 @@ const routeModule = new AppPageRouteModule({
 
 /***/ }),
 
+/***/ 50020:
+/***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
+
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 31232, 23));
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 52987, 23));
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 50831, 23));
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 56926, 23));
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 44282, 23));
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 16505, 23))
+
+/***/ }),
+
 /***/ 50600:
 /***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
 
@@ -495,7 +507,7 @@ function Booking() {
 var __webpack_require__ = require("../../webpack-runtime.js");
 __webpack_require__.C(exports);
 var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
-var __webpack_exports__ = __webpack_require__.X(0, [587,191,398,395], () => (__webpack_exec__(46596)));
+var __webpack_exports__ = __webpack_require__.X(0, [587,191,398,617], () => (__webpack_exec__(46596)));
 module.exports = __webpack_exports__;
 
 })();
--- server/app/page.js
+++ server/app/page.js
@@ -333,6 +333,18 @@ const routeModule = new AppPageRouteModule({
 
 /***/ }),
 
+/***/ 61115:
+/***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
+
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 52987, 23));
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 50831, 23));
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 56926, 23));
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 44282, 23));
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 16505, 23));
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 31232, 23))
+
+/***/ }),
+
 /***/ 28883:
 /***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
 
@@ -649,7 +661,7 @@ function Home() {
 var __webpack_require__ = require("../webpack-runtime.js");
 __webpack_require__.C(exports);
 var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
-var __webpack_exports__ = __webpack_require__.X(0, [587,191,451,395], () => (__webpack_exec__(90540)));
+var __webpack_exports__ = __webpack_require__.X(0, [587,191,451,617], () => (__webpack_exec__(90540)));
 module.exports = __webpack_exports__;
 
 })();
