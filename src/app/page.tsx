a4de5a7dd6224dd723ac9f5a8b9c45be9267982c
Phumipat Chaiprasertsud <phumipat.c@outlook.com>
2023-10-03 23:32:46 +0700

    Assignment 08

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,13 @@ import styles from './page.module.css'
 import ProductCard from '@/components/ProductCard'
 import Banner from '@/components/banner'
 import CardPanel from '@/components/CardPanel'
+import PromoteCard from '@/components/PromoteCard'
 
 export default function Home() {
   return (
     <main>
       <Banner />
+      <PromoteCard />
     </main>
   )
 }
